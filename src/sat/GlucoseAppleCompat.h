bee522151f12109ecf7ad7344d4ce42b42b9ba50
Noam Cohen <noam.chn1@gmail.com>
2026-07-13 19:02:10 +0200

    Fix C2RTL CMake CI builds

---
+++ src/sat/GlucoseAppleCompat.h
@@ -0,0 +1,11 @@
+// Copyright 2026 keplertech.io
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+#if defined(__APPLE__)
+#include <sys/_types/_size_t.h>
+#endif
+#include <stddef.h>
+#include <cstddef>
+
